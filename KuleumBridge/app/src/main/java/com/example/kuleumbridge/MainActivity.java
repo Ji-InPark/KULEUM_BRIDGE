737f3ceb74566ccc52190e71fdeacd67bb90b6dd
JinPark <jin225675@naver.com>
2021-11-15 22:53:48 +0900

    modify file i/o for calendar

--- KuleumBridge/app/src/main/java/com/example/kuleumbridge/MainActivity.java
+++ KuleumBridge/app/src/main/java/com/example/kuleumbridge/MainActivity.java
@@ -9,7 +9,6 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Base64;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -19,11 +18,7 @@ import android.widget.TextView;
 import com.google.android.material.tabs.TabLayout;
 
 import java.io.FileInputStream;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Random;
-
-// test
 
 public class MainActivity extends AppCompatActivity {
      // User의 정보들을 저장할 객체
@@ -154,27 +149,20 @@ public class MainActivity extends AppCompatActivity {
                         }
                     });
 
+                    // 로딩 애니메이션 종료
                     customProgress.dismiss();
 
+                    // 작은 캘린더에 출력하는 부분
                     calenderTV = findViewById(R.id.calendarview);
                     mainAlarm mA = new mainAlarm();
                     String today = mA.getTime();
                     System.out.println(today);
-                    String userID = mA.sendID(); // sendID()가 null값을 가져옴
-                    String todayfile = "" + userID + today + ".txt"; // "null2021-11-09.txt" 이렇게 저장됨
-                    String filedata = null;
-                    FileInputStream fis = null;//FileStream fis 변수
-                    fis = openFileInput(todayfile); // todayfile 값이 위처럼 생겼다보니 당연히 안열림.
 
-                    /* 당연히 이부분은 파일 오픈에 실패했으므로 정상적으로 수행되지 않음.*/
-                    byte[] fileData = new byte[fis.available()];
-                    fis.read(fileData);
-                    for (int i = 0; i<fileData.length; i++) {
-                        System.out.print(fileData[i]);
-                    }
-                    fis.close();
-                    filedata = new String(fileData);
-                    //calenderTV.setText(today+"/n"+filedata);
+                    pref = getSharedPreferences(today ,MODE_PRIVATE); // 날짜를 기준으로 여는 것
+
+                    String fileData = pref.getString("input", "오늘 할 일이 존재하지 않습니다."); // fileData 변수에 저장된 것을 저장
+
+                    calenderTV.setText(today + "\n" + fileData); // 로그인 후 작은 캘린더 화면에 출력
 
                 }
                 catch (Exception e)
@@ -193,7 +181,6 @@ public class MainActivity extends AppCompatActivity {
             public void callback_fail() {
                 // 연결 실패시 작동
                 // 애니메이션 동작 중단
-                // 적정한 화면으로 전환
                 customProgress.dismiss();
             }
         });
