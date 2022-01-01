9c2dd9a2796a9063946111a027de64187e9e8545
wonniiii <eve652358@gmail.com>
2022-01-01 17:20:32 +0900

    Add TastePlace

---
+++ KuleumBridge/app/src/main/java/com/example/kuleumbridge/TasteInfoContent.java
@@ -0,0 +1,85 @@
+package com.example.kuleumbridge;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+class TasteInfoContent extends Fragment implements OnMapReadyCallback { //TastePlaceInfo 화면 구성
+
+    View rootView;
+    MapView mapView;
+
+    public TasteInfoContent() {
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.taste_info_content, container, false);
+        mapView = (MapView) rootView.findViewById(R.id.mapview);
+        mapView.onCreate(savedInstanceState);
+
+        mapView.getMapAsync(this);
+
+        return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        MapsInitializer.initialize(this.getActivity());
+
+        // Updates the location and zoom of the MapView
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(37.5425241, 127.073699), 17);
+
+        googleMap.animateCamera(cameraUpdate);
+
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(37.5425241, 127.073699))
+                .title("건국대학교"));
+    }
+}
---
+++ KuleumBridge/app/src/main/java/com/example/kuleumbridge/TastePlaceList.java
@@ -0,0 +1,75 @@
+package com.example.kuleumbridge;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+
+public class TastePlaceList extends AppCompatActivity { //맛집 리스트 출력
+
+    ListView list_excel;
+    ArrayAdapter<String> arrayAdapter;
+    String kinds[]= new String[200];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.taste_list);
+        list_excel = (ListView)findViewById(R.id.list_excel);
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        Excel();
+
+    }
+    public void Excel() {
+        Workbook workbook = null;
+        Sheet sheet = null;
+
+        try {
+            InputStream inputStream = getBaseContext().getResources().getAssets().open("place.xls");
+            workbook = Workbook.getWorkbook(inputStream);
+            sheet = workbook.getSheet(0);
+            int  RowEnd = sheet.getColumn(0).length -1;
+
+            for(int row = 1;row <= RowEnd;row++) {
+//                kinds[row]=sheet.getCell(0, row).getContents();
+//                if(kinds[row].equals("한식")) {
+//                    String excelload = kinds[row];
+                    String excelload = sheet.getCell(1, row).getContents();
+                    arrayAdapter.add(excelload);
+                }
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (BiffException e) {
+            e.printStackTrace();
+        } finally {
+            list_excel.setAdapter(arrayAdapter);
+
+            workbook.close();
+
+            list_excel.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    Intent intent = new Intent(TastePlaceList.this,TastePlaceInfo.class);
+                    startActivity(intent);
+                }
+            });
+        }
+    }
+
+
+    }
