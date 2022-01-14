dc962e9f0659633642439c22cb806282ee2f9f10
wonniiii <eve652358@gmail.com>
2022-01-14 23:19:55 +0900

    complete TastePlaceActivity

--- KuleumBridge/app/src/main/java/com/example/kuleumbridge/TastePlaceActivity.java
+++ KuleumBridge/app/src/main/java/com/example/kuleumbridge/TastePlaceActivity.java
@@ -19,12 +19,15 @@ import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.internal.ICameraUpdateFactoryDelegate;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
 import java.io.InputStream;
 
+import jxl.Cell;
+import jxl.NumberCell;
 import jxl.Sheet;
 import jxl.Workbook;
 import jxl.read.biff.BiffException;
@@ -32,11 +35,13 @@ import jxl.read.biff.BiffException;
 public class TastePlaceActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    String name[]= new String[200];
-    String address[]= new String[200];
-    double latitude[]= new double[200]; //위도
+    String kind[] = new String[200]; //종류
+    String name[]= new String[200]; //상호명
+    String address[]= new String[200]; //주소
+    double latitude[] = new double[200]; //위도
     double longitude[]= new double[200]; //경도
-
+    Cell[] test;
+    int RowEnd=0;
 
     public void Excel() {
         Workbook workbook = null;
@@ -46,16 +51,27 @@ public class TastePlaceActivity extends AppCompatActivity implements OnMapReadyC
             InputStream inputStream = getBaseContext().getResources().getAssets().open("place.xls");
             workbook = Workbook.getWorkbook(inputStream);
             sheet = workbook.getSheet(0);
-            int  RowEnd = sheet.getRows()-1;
-            System.out.println(RowEnd);
+            RowEnd = sheet.getRows()-1;
+
 
 
             for(int row = 1;row <= RowEnd;row++) {
 
+                kind[row]=sheet.getCell(0,row).getContents();
+
                 name[row]=sheet.getCell(1, row).getContents();
                 address[row]=sheet.getCell(2,row).getContents();
-                latitude[row]=Double.parseDouble(sheet.getCell(3,row).getContents());
-                longitude[row] = Double.parseDouble(sheet.getCell(4,row).getContents());
+
+                NumberCell latitude2 = (NumberCell)sheet.getCell(3,row);
+                latitude2.getNumberFormat().setMaximumFractionDigits(5);
+                latitude[row]= Double.parseDouble(latitude2.getContents()); //위도
+
+                NumberCell longitude2 = (NumberCell)sheet.getCell(4,row);
+                longitude2.getNumberFormat().setMaximumFractionDigits(5);
+                longitude[row] = Double.parseDouble(longitude2.getContents()); //경도
+
+
+
 
             }
 
@@ -79,26 +95,44 @@ public class TastePlaceActivity extends AppCompatActivity implements OnMapReadyC
 
     @Override
     public void onMapReady(final GoogleMap googleMap) {
-
-
-
         mMap = googleMap;
+        String kinds = "";
 
 
-//건대 마커
-//        MarkerOptions markerOptions = new MarkerOptions();
-//        markerOptions.position(new LatLng(37.5425241,127.073699)).title("건국대학교").snippet("창조적 혁신으로 미래를 선도하는 대학");
-//
-//        mMap.addMarker(markerOptions);
-
-        for(int i =1; i< name.length; i++) {
+        for(int i =1; i< RowEnd; i++) {
             MarkerOptions marker = new MarkerOptions();
             marker.position(new LatLng(latitude[i],longitude[i])).title(name[i]).snippet(address[i]);
 
+            if (kind[i].contains("한식")) {
+                marker.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+            }else if(kind[i].contains("분식")) {
+                marker.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE));
+            } else if(kind[i].contains("카페/디저트")) {
+                marker.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW));
+            } else if(kind[i].contains("돈까스/회/일식")) {
+                marker.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+            } else if(kind[i].contains("치킨/햄버거")) {
+                marker.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+            } else if(kind[i].contains("아시안/양식")) {
+                marker.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+            }else if(kind[i].contains("중식")) {
+                marker.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET));
+
+            }else if(kind[i].contains("고기")) {
+                marker.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
+            }else if(kind[i].contains("술집")) {
+                marker.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN));
+            }
+
             mMap.addMarker(marker);
+
+
+
+
+
         } //다중 마커 추가
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.5425241,127.073699), 15));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.5425241,127.073699), 16));
 
 
     }
