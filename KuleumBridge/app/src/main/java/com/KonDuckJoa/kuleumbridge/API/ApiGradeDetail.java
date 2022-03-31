a4b7336f12810e7bfc1620af4f1d31f0f277210f
Jin Park <jin225675@naver.com>
2022-03-31 22:07:21 +0900

    refactor ApiGradeDetail according to code convention

--- KuleumBridge/app/src/main/java/com/KonDuckJoa/kuleumbridge/API/ApiGradeDetail.java
+++ KuleumBridge/app/src/main/java/com/KonDuckJoa/kuleumbridge/API/ApiGradeDetail.java
@@ -15,7 +15,7 @@ import okhttp3.Response;
 
 public class ApiGradeDetail extends AsyncTask<String, String, Boolean> {
     private String std_num, year, shtm, id, result;
-    private CallBack cb;
+    private CallBack callBackb;
 
     public ApiGradeDetail(String std_num, String year, String shtm, String id, CallBack cb)
     {
@@ -23,42 +23,42 @@ public class ApiGradeDetail extends AsyncTask<String, String, Boolean> {
         this.year = year;
         this.shtm = shtm;
         this.id = id;
-        this.cb = cb;
+        this.callBackb = cb;
     }
 
     @Override
-    protected void onPostExecute(java.lang.Boolean success) {
+    protected void onPostExecute(java.lang.Boolean success)
+    {
         super.onPostExecute(success);
 
         if(success)
         {
-            cb.callback_success(result);
+            callBackb.callback_success(result);
         }
-        else
-        {
-            return;
-        }
-
     }
 
     @Override
-    protected java.lang.Boolean doInBackground(String... strings) {
-        final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    protected java.lang.Boolean doInBackground(String... strings)
+    {
+        final MediaType jsonType = MediaType.parse("application/json; charset=utf-8");
 
-        OkHttpClient client = new OkHttpClient();
+        OkHttpClient okHttpClient = new OkHttpClient();
 
-        JSONObject json = new JSONObject();
-        try {
-            json.put("std_num", std_num);
-            json.put("year", year);
-            json.put("shtm", shtm);
-            json.put("sub_id", id);
-        } catch (JSONException e) {
+        JSONObject parameterJson = new JSONObject();
+        try
+        {
+            parameterJson.put("std_num", std_num);
+            parameterJson.put("year", year);
+            parameterJson.put("shtm", shtm);
+            parameterJson.put("sub_id", id);
+        }
+        catch (JSONException e)
+        {
             e.printStackTrace();
         }
 
         // rest api 로그인 post로 보냄
-        RequestBody body = RequestBody.create(JSON, json.toString());
+        RequestBody body = RequestBody.create(jsonType, parameterJson.toString());
         Request request = new Request.Builder()
                 .url("http://3.37.235.212:5000/grade/detail")
                 .addHeader("Connection", "close")
@@ -67,24 +67,19 @@ public class ApiGradeDetail extends AsyncTask<String, String, Boolean> {
 
         Response response;
 
-        try {
-            response = client.newCall(request).execute();
+        try
+        {
+            response = okHttpClient.newCall(request).execute();
             result = response.body().string();
 
-            if (result.contains("ERRMSGINFO")) {
-                return false;
-            } else {
-                return true;
-            }
-        } catch (Exception e) {
+            return !result.contains("ERRMSGINFO");
+        }
+        catch (Exception e)
+        {
             e.printStackTrace();
         }
 
         return true;
-    }
-
-    public java.lang.String getResult() {
-        return result;
     }
 }
 
