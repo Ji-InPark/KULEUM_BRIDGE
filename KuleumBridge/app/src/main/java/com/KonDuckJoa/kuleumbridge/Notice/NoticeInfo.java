63c262e9eea4a90a38b28e66f8533fcb24170279
Jin Park <jin225675@naver.com>
2022-03-31 22:37:26 +0900

    refactor NoticeInfo according to code convention

--- KuleumBridge/app/src/main/java/com/KonDuckJoa/kuleumbridge/Notice/NoticeInfo.java
+++ KuleumBridge/app/src/main/java/com/KonDuckJoa/kuleumbridge/Notice/NoticeInfo.java
@@ -20,7 +20,8 @@ public class NoticeInfo {
 
     private ArrayList<Notice>[] notices;
 
-    private NoticeInfo() {
+    private NoticeInfo()
+    {
         notices = new ArrayList[7];
 
         for(int i = 0; i < 7; i++) notices[i] = new ArrayList<>();
@@ -35,21 +36,28 @@ public class NoticeInfo {
     // SUBJECT는 글 제목
     // ARTICLE_ID는 각 게시글 URL에 들어있는 게시글 고유의 ID
 
-    public void setNoticeInfo(String response_string_notice, String notice_category) {
-        try {
-            JSONObject temp = new JSONObject(response_string_notice);
-            JSONArray DS_LIST = temp.getJSONArray("DS_LIST");
+    public void setNoticeInfo(String responseNotice, String noticeCategory)
+    {
+        try
+        {
+            JSONObject responseNoticeJson = new JSONObject(responseNotice);
+
+            JSONArray noticeJsonArray = responseNoticeJson.getJSONArray("DS_LIST");
+
             for (int i = 0; i < 5; i++)
             {
-                JSONObject notice = DS_LIST.getJSONObject(i);
-                String article_id = notice.getString("ARTICLE_ID");
-                notices[NoticeHandler.getIndex(notice_category)].add(new Notice(
-                        notice.getString("POSTED_DT"),
-                        notice.getString("SUBJECT"), article_id,
-                        NoticeHandler.getLink(notice_category, article_id)));
+                JSONObject noticeJson = noticeJsonArray.getJSONObject(i);
+
+                String articleId = noticeJson.getString("ARTICLE_ID");
+
+                notices[NoticeHandler.getIndex(noticeCategory)].add(new Notice(
+                        noticeJson.getString("POSTED_DT"),
+                        noticeJson.getString("SUBJECT"), articleId,
+                        NoticeHandler.getLink(noticeCategory, articleId)));
             }
         }
-        catch (Exception e) {
+        catch (Exception e)
+        {
             e.printStackTrace();
         }
     }
