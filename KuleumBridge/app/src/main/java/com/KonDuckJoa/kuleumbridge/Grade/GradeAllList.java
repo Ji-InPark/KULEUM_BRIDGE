d31ea7d43f87c9e573868c69cbe55a86611e5a1f
Jin Park <jin225675@naver.com>
2022-04-04 00:33:52 +0900

    refactor about GradeAll logics

--- KuleumBridge/app/src/main/java/com/KonDuckJoa/kuleumbridge/Grade/GradeAllList.java
+++ KuleumBridge/app/src/main/java/com/KonDuckJoa/kuleumbridge/Grade/GradeAllList.java
@@ -21,12 +21,13 @@ import java.util.Arrays;
 
 
 public class GradeAllList extends Fragment {
-    ArrayList<Grade> gradeAllArray;
-    ArrayList<String> yearArray; //년도 + 학기
-    ArrayList<String> divisionArray;  //이수구분
-    ArrayList<String> nameArray; //이름
-    ArrayList<String> gradeCountArray; //학점
-    ArrayList<String> gradeRateArray; //성적
+    private static ArrayList<Grade> gradeAllArray;
+
+    private static ArrayList<String> yearArray = new ArrayList<>();       //년도 + 학기
+    private static ArrayList<String> divisionArray = new ArrayList<>();   //이수구분
+    private static ArrayList<String> nameArray = new ArrayList<>();       //이름
+    private static ArrayList<String> gradeCountArray = new ArrayList<>(); //학점
+    private static ArrayList<String> gradeRateArray = new ArrayList<>();  //성적
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -38,16 +39,8 @@ public class GradeAllList extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         View rootView;
-
-        String selectedYear = "";
-
-        StringBuilder year = new StringBuilder();
-        StringBuilder division = new StringBuilder();
-        StringBuilder name = new StringBuilder();
-        StringBuilder gradeCount = new StringBuilder();
-        StringBuilder gradeRate = new StringBuilder();
-
         TableLayout tableLayout;
+        String selectedYear = "";
 
         // Inflate the layout for this fragment
         rootView = inflater.inflate(R.layout.grade_detail_list, container, false);
@@ -55,45 +48,10 @@ public class GradeAllList extends Fragment {
 
         if (getArguments() != null)
         {
-            gradeAllArray = UserInfo.getInstance().getGradeAll();
-            selectedYear = getArguments().getString("tabb"); //클릭한 탭 내용 받아옴(ex. 2021년 1학기)
+            selectedYear = getArguments().getString("tabName"); //클릭한 탭 내용 받아옴(ex. 2021년 1학기)
         }
 
-        for (int i = 0; i < gradeAllArray.size(); i++)
-        {
-            year.append(gradeAllArray.get(i).getCompletedYear()).append(" ").append(gradeAllArray.get(i).getSemester()).append(",");
-            division.append(gradeAllArray.get(i).getCompletedDivision()).append(",");
-            name.append(gradeAllArray.get(i).getSubjectName()).append(",");
-            gradeCount.append(gradeAllArray.get(i).getGradeCount()).append(",");
-            gradeRate.append(gradeAllArray.get(i).getGradeRate()).append(",");
-        }
-
-        yearArray = new ArrayList<>(Arrays.asList(year.toString().split(",")));
-        divisionArray = new ArrayList<>(Arrays.asList(division.toString().split(",")));
-        nameArray = new ArrayList<>(Arrays.asList(name.toString().split(",")));
-        gradeCountArray = new ArrayList<>(Arrays.asList(gradeCount.toString().split(",")));
-        gradeRateArray = new ArrayList<>(Arrays.asList(gradeRate.toString().split(",")));
-
-        //Arraylist에서 불필요한 요소들 없애주는 과정
-        yearArray.remove(yearArray.size() - 1);
-        divisionArray.remove(divisionArray.size() - 1);
-        nameArray.remove(nameArray.size() - 1);
-        gradeCountArray.remove(gradeCountArray.size() - 1);
-        gradeRateArray.remove(gradeRateArray.size() - 1);
-
-        for (int i = 0; i < yearArray.size() - 1; i++)
-        {
-            if (yearArray.get(i).contains("소계"))
-            {
-                yearArray.remove(i);
-                divisionArray.remove(i);
-                nameArray.remove(i);
-                gradeCountArray.remove(i);
-                gradeRateArray.remove(i);
-            }
-        }
-
-        for (int i = 0; i < yearArray.size() - 1; i++)
+        for (int i = 0; i < yearArray.size(); i++)
         {
             //클릭한 탭내용과 일치하는 성적만 불러오는 과정(ex. 2021년 2학기 클릭시, 해당 학기 성적만 불러옴)
             if (yearArray.get(i).contains(selectedYear))
@@ -141,14 +99,33 @@ public class GradeAllList extends Fragment {
         return rootView;
     }
 
+    public static void setGradeAllInfo()
+    {
+        gradeAllArray = UserInfo.getInstance().getGradeAll();
+
+        // 소계 제외
+        for (int i = 0; i < UserInfo.getInstance().getGradeAll().size(); i++)
+        {
+            if (gradeAllArray.get(i).getSemester().contains("소계")) continue;
+
+            yearArray.add(gradeAllArray.get(i).getCompletedYear() + " " + gradeAllArray.get(i).getSemester());
+            divisionArray.add(gradeAllArray.get(i).getCompletedDivision());
+            nameArray.add(gradeAllArray.get(i).getSubjectName());
+            gradeCountArray.add(gradeAllArray.get(i).getGradeCount());
+            gradeRateArray.add(gradeAllArray.get(i).getGradeRate());
+        }
+    }
+
     private TextView getTextViewWithSettings()
     {
         TextView textView = new TextView(getActivity());
+
         textView.setTextSize(19);
         textView.setTextColor(Color.parseColor("#000000"));
         textView.setWidth(0);
         textView.setPadding(0,0,10,70);
         textView.setSelected(true);
+
         return textView;
     }
 }
